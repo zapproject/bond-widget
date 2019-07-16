2e172c7b9a006c976ba392086256cde19fbfd914
Pavel B <pavel.b@smartum.pro>
2019-07-16 16:25:39 +0300

    apply new styles with theme

--- src/app/bond-widget/bond-widget/bond-widget.component.ts
+++ src/app/bond-widget/bond-widget/bond-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, Input, ChangeDetectorRef, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, ViewEncapsulation, Input, ChangeDetectorRef, OnChanges, SimpleChanges, AfterViewInit, ElementRef } from '@angular/core';
 import { SubscriberService } from 'src/app/subscriber-service/subscriber.service';
 import { take } from 'rxjs/operators';
 import { ColorThemeName, colorThemes } from 'src/app/color-themes';
@@ -8,7 +8,7 @@ import { ColorThemeName, colorThemes } from 'src/app/color-themes';
   styleUrls: ['./bond-widget.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class BondWidgetComponent implements OnChanges {
+export class BondWidgetComponent implements OnChanges, AfterViewInit {
 
   @Input() address: string;
   @Input() endpoint: string;
@@ -20,19 +20,25 @@ export class BondWidgetComponent implements OnChanges {
   constructor(
     public zap: SubscriberService,
     private cd: ChangeDetectorRef,
+    private el: ElementRef,
   ) {}
 
   private updateTheme() {
+    if (!this.el) return;
     const theme = colorThemes[this.theme];
     if (!theme) return;
     const body = document.documentElement;
-    Object.keys(theme).forEach(k =>
-      document.documentElement.style.setProperty(`--${k}`, theme[k])
-    );
+    Object.keys(theme).forEach(k => {
+      this.el.nativeElement.style.setProperty(`--${k}`, theme[k]);
+    });
   }
 
-  ngOnChanges(chages: SimpleChanges): void {
-    if (chages.theme.firstChange || chages.theme.currentValue !== chages.theme.previousValue) {
+  ngAfterViewInit() {
+    this.updateTheme();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.theme && (changes.theme.firstChange || changes.theme.currentValue !== changes.theme.previousValue)) {
       this.updateTheme();
     }
     if (!this.endpoint || !this.address) {
