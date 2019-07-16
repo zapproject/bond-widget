1ab31e29e3b77b733d957a9efe038aeef0119df1
Pavel B <pavel.b@smartum.pro>
2019-07-16 10:49:49 +0300

    make css custom props for themes, add themes to typescript

--- src/app/bond-widget/bond-widget/bond-widget.component.ts
+++ src/app/bond-widget/bond-widget/bond-widget.component.ts
@@ -1,17 +1,18 @@
-import { Component, ViewEncapsulation, Input, ChangeDetectorRef, OnChanges } from '@angular/core';
-import { BondService } from 'src/app/bond-service/bond.service';
+import { Component, ViewEncapsulation, Input, ChangeDetectorRef, OnChanges, SimpleChanges } from '@angular/core';
 import { SubscriberService } from 'src/app/subscriber-service/subscriber.service';
-import { ProviderService } from 'src/app/provider-service/provider.service';
 import { take } from 'rxjs/operators';
+import { ColorThemeName, colorThemes } from 'src/app/color-themes';
 
 @Component({
   templateUrl: './bond-widget.component.html',
+  styleUrls: ['./bond-widget.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class BondWidgetComponent implements OnChanges {
 
   @Input() address: string;
   @Input() endpoint: string;
+  @Input() theme: ColorThemeName = 'green';
   @Input() interface: 'standard' | 'bond' = 'standard';
 
   widgetType: 'TOKEN' | 'PROVIDER';
@@ -21,7 +22,19 @@ export class BondWidgetComponent implements OnChanges {
     private cd: ChangeDetectorRef,
   ) {}
 
-  ngOnChanges(): void {
+  private updateTheme() {
+    const theme = colorThemes[this.theme];
+    if (!theme) return;
+    const body = document.documentElement;
+    Object.keys(theme).forEach(k =>
+      document.documentElement.style.setProperty(`--${k}`, theme[k])
+    );
+  }
+
+  ngOnChanges(chages: SimpleChanges): void {
+    if (chages.theme.firstChange || chages.theme.currentValue !== chages.theme.previousValue) {
+      this.updateTheme();
+    }
     if (!this.endpoint || !this.address) {
       this.widgetType = null;
       setTimeout(() => { this.cd.detectChanges(); });
@@ -30,11 +43,9 @@ export class BondWidgetComponent implements OnChanges {
     this.zap.isToken(this.address, this.endpoint).pipe(
       take(1),
     ).subscribe(isToken => {
-      console.log('isToken', isToken);
       this.widgetType = isToken ? 'TOKEN' : 'PROVIDER';
       setTimeout(() => { this.cd.detectChanges(); });
     }, error => {
-      console.log('isToken', error);
       this.widgetType = null;
       setTimeout(() => { this.cd.detectChanges(); });
     });
